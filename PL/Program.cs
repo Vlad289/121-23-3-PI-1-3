5ee25fd1528ed8416181aa80a3b754125102562d
Goplap <alexey.antsybor@gmail.com>
2025-05-14 18:21:27 +0300

    update program

--- PL/Program.cs
+++ PL/Program.cs
@@ -1,80 +1,130 @@
 ﻿using BLL.Interfaces;
 using BLL.Services;
 using DAL;
-using DAL.Context;
 using DAL.Interfaces;
-using DAL.Models;
 using DAL.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using PL.Controllers;
 using PL.UI;
+using System;
 
 namespace PL
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             // Configure services
-            var services = new ServiceCollection();
-            ConfigureServices(services);
-            var serviceProvider = services.BuildServiceProvider();
+            var serviceProvider = ConfigureServices();
 
-            try
-            {
-                // Create and migrate database
-                using (var scope = serviceProvider.CreateScope())
-                {
-                    var dbContext = scope.ServiceProvider.GetRequiredService<OnlineShopDbContext>();
-                    dbContext.Database.Migrate();
-                    Console.WriteLine("Database connection successful.");
-                }
-
-                // Run the main menu
-                var mainMenuUI = serviceProvider.GetRequiredService<MainMenuUI>();
-                mainMenuUI.ShowMenu();
-            }
-            catch (Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Application error: {ex.Message}");
-                Console.WriteLine(ex.StackTrace);
-                Console.ResetColor();
-                Console.ReadKey();
-            }
+            // Start the application
+            var mainMenu = new MainMenu(serviceProvider);
+            mainMenu.Show();
         }
 
-        private static void ConfigureServices(IServiceCollection services)
+        private static IServiceProvider ConfigureServices()
         {
-            // Database
+            var services = new ServiceCollection();
+
+            // Register DbContext
             services.AddDbContext<OnlineShopDbContext>(options =>
                 options.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=OnlineShopDb;Trusted_Connection=True;TrustServerCertificate=True;"));
 
-            // Repositories
+            // Register repositories
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IOrderRepository, OrderRepository>();
             services.AddScoped<IOrderItemRepository, OrderItemRepository>();
 
-            // Services
+            // Register services
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<IOrderService, OrderService>();
             services.AddScoped<IOrderItemService, OrderItemService>();
 
-            // Controllers
+            // Register controllers
             services.AddScoped<UserController>();
             services.AddScoped<ProductController>();
             services.AddScoped<OrderController>();
             services.AddScoped<OrderItemController>();
 
-            // UI
-            services.AddScoped<MainMenuUI>();
+            // Register UI classes
             services.AddScoped<UserUI>();
             services.AddScoped<ProductUI>();
             services.AddScoped<OrderUI>();
-            services.AddTransient<CurrentUserManager>();
+            services.AddScoped<MainMenu>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+
+    /// <summary>
+    /// Main menu for the application
+    /// </summary>
+    public class MainMenu : BaseUI
+    {
+        private readonly UserUI _userUI;
+        private readonly ProductUI _productUI;
+        private readonly OrderUI _orderUI;
+
+        public MainMenu(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            _userUI = GetController<UserUI>();
+            _productUI = GetController<ProductUI>();
+            _orderUI = GetController<OrderUI>();
+        }
+
+        public void Show()
+        {
+            bool exit = false;
+
+            // Display welcome message
+            Console.Clear();
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("*          ONLINE SHOP MANAGEMENT           *");
+            Console.WriteLine("*********************************************");
+            Console.WriteLine();
+            PressAnyKeyToContinue();
+
+            while (!exit)
+            {
+                DisplayHeader("Main Menu");
+                Console.WriteLine("1. User Management");
+                Console.WriteLine("2. Product Management");
+                Console.WriteLine("3. Order Management");
+                Console.WriteLine("0. Exit Application");
+
+                int choice = GetIntInput("\nEnter your choice: ", 0, 3);
+
+                switch (choice)
+                {
+                    case 1:
+                        _userUI.ShowMenu();
+                        break;
+                    case 2:
+                        _productUI.ShowMenu();
+                        break;
+                    case 3:
+                        _orderUI.ShowMenu();
+                        break;
+                    case 0:
+                        exit = true;
+                        break;
+                }
+            }
+
+            // Display goodbye message
+            Console.Clear();
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("*       Thank you for using our app!        *");
+            Console.WriteLine("*********************************************");
+            Console.WriteLine();
+        }
+
+        public override void ShowMenu()
+        {
+            Show();
         }
     }
 }
